ba8e3770f050653cb1e30b50dabc223a5dc5fcfd
ndxbxrme <lewis_the_cat@hotmail.com>
2014-09-29 09:36:06 +0100

    myfeeds screen

--- app/scripts/directives/myfeedsitem.js
+++ app/scripts/directives/myfeedsitem.js
@@ -8,11 +8,35 @@
  */
 angular.module('podcaddyApp')
   .directive('myfeedsitem', function () {
-    return {
-      template: '<div></div>',
-      restrict: 'E',
-      link: function postLink(scope, element, attrs) {
-        element.text('this is the myfeedsitem directive');
-      }
-    };
-  });
+        return {
+            templateUrl: '/views/myfeedsitem.html',
+            restrict: 'AE',
+            scope: {
+                feed: '='   
+            },
+            replace: true,
+            link: function postLink(scope, element) {
+
+                $('<img/>').attr('src', scope.feed.data.image).load(function(){
+                    element.css('background-image', 'url(' + scope.feed.data.image + ')'); 
+                }).error(function(){
+                    $(this).remove();
+                    element.css('background-image', 'url(http://unsplash.it/200/200?image=' + scope.feed.id +')');
+                });
+                if(!scope.feed.data.image) {
+                    console.log('yo');
+                    element.css('background-image', 'url(http://unsplash.it/200/200?image=' + scope.feed.id +')');   
+                }
+                
+                scope.toggle = function(){
+                    scope.toggling = true;
+                    $http.post('/api/subs/toggle', {
+                        feedid: scope.feed.id
+                    }).success(function(data){
+                        scope.toggling = false;
+                        scope.feed.subscribed = data.subscribed ? [true] : [];
+                    });
+                };
+            }
+        };
+    });
