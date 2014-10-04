b80dc571c31ea28c21b81cd78b69d7dbca930f3f
ndxbxrme <lewis_the_cat@hotmail.com>
2014-10-04 21:11:43 +0100

    player url and item description

--- app/scripts/services/pageplayer.js
+++ app/scripts/services/pageplayer.js
@@ -137,7 +137,7 @@ angular.module('podcaddyApp')
           }
           var thisSound = sm.createSound({
             id:'item_' + item.id,
-            url:decodeURI(item.data.enclosure[0].$.url),
+            url:decodeURI(item.data.url),
             onplay: self.events.play,
             onstop: self.events.stop,
             onpause: self.events.pause,
