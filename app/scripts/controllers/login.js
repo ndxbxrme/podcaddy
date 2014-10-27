f390459bd23856824d5aa3ae862e26b49ba41371
ndxbxrme <lewis_the_cat@hotmail.com>
2014-10-27 09:30:01 +0000

    build

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -18,10 +18,12 @@ angular.module('podcaddyApp')
     $scope.submit = function() {
         $http.post('/authenticate', angular.extend($scope.user, {password:doHash($scope.user.password)}))
         .success(function(data) {
+            $rootScope.loggedIn = true;
             $window.sessionStorage.token = data.token;
             $location.path('/');
         })
         .error(function(error){
+            $rootScope.loggedIn = false;
             delete $window.sessionStorage.token;
             $scope.message = error;
         });
@@ -34,14 +36,17 @@ angular.module('podcaddyApp')
           $http.post('/signup', angular.extend($scope.user, {password:doHash($scope.user.password)}))
           .success(function(data) {
               if(!data.error) {
+                $rootScope.loggedIn = true;
                 $window.sessionStorage.token = data.token;
                 $location.path('/allfeeds');
               } else {
+                $rootScope.loggedIn = false;
                 $scope.message = data.message; 
                 $scope.user.password = '';
               }
           })
           .error(function(error){
+              $rootScope.loggedIn = false;
               delete $window.sessionStorage.token;
               $scope.message = error;
           });   
