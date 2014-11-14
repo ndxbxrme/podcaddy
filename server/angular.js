f56a7a0ae49a593a259abd7827d716ec1aab74e4
ndxbxrme <lewis_the_cat@hotmail.com>
2014-11-14 23:37:24 +0000

    better build

--- server/angular.js
+++ server/angular.js
@@ -2,13 +2,13 @@
 var gzippo = require('gzippo');
 
 module.exports = function(app) {
-  app.use('/scripts', gzippo.staticGzip(__dirname + '/../dist/scripts'));
-  app.use('/images', gzippo.staticGzip(__dirname + '/../dist/images'));
-  app.use('/styles', gzippo.staticGzip(__dirname + '/../dist/styles'));
-  app.use('/views', gzippo.staticGzip(__dirname + '/../dist/views'));
-  app.use('/swf', gzippo.staticGzip(__dirname + '/../dist/swf'));
-  app.use('/fonts', gzippo.staticGzip(__dirname + '/../dist/fonts'));
+  app.use('/scripts', gzippo.staticGzip(__dirname + '../../dist/scripts'));
+  app.use('/images', gzippo.staticGzip(__dirname + '../../dist/images'));
+  app.use('/styles', gzippo.staticGzip(__dirname + '../../dist/styles'));
+  app.use('/views', gzippo.staticGzip(__dirname + '../../dist/views'));
+  app.use('/swf', gzippo.staticGzip(__dirname + '../../dist/swf'));
+  app.use('/fonts', gzippo.staticGzip(__dirname + '../../dist/fonts'));
   app.all('/*', function(req, res) {
-    res.sendFile('index.html', {root: __dirname + '/../dist'});
+    res.sendFile('index.html', {root: __dirname + '../../dist'});
   });   
 };
