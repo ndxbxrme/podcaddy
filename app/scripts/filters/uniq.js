49ff02a8da26ac1a0f6a20c975dc7aa1335ea3a5
ndxbxrme <lewis_the_cat@hotmail.com>
2016-08-28 22:15:36 +0100

    uniq

---
+++ app/scripts/filters/uniq.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc filter
+ * @name myApp.filter:listened
+ * @function
+ * @description
+ * # listened
+ * Filter in the myApp.
+ */
+angular.module('myApp')
+  .filter('uniq', function () {
+    return function (input) {
+      return _.uniq(input, function(i) {
+        return i.url;
+      });
+    };
+  });
