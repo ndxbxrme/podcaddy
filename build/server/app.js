97c9377852f7c37d9abc5bf501c7ccd33610dd4f
ndxbxrme <lewis_the_cat@hotmail.com>
2017-01-08 10:56:29 +0000

    host

--- build/server/app.js
+++ build/server/app.js
@@ -5,7 +5,8 @@
   ndx = require('ndx-server').config({
     database: 'pc',
     tables: ['u', 'f', 's', 'i', 'l'],
-    userTable: 'u'
+    userTable: 'u',
+    host: 'www.podcaddy.co.uk'
   }).controller(require('./controllers/feeds.js')).start();
 
 }).call(this);
--- src/server/app.coffee
+++ src/server/app.coffee
@@ -5,5 +5,6 @@ ndx = require 'ndx-server'
   database: 'pc'
   tables: ['u','f','s','i','l']
   userTable: 'u'
+  host: 'www.podcaddy.co.uk'
 .controller require('./controllers/feeds.js')
 .start()
